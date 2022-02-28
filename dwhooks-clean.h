4a881a6b3f52b73d06766bd8343deadbd0ecc137
Damadux <dpiche.veg@gmail.com>
2022-02-28 15:55:43 -0500

    Added header file for dwhooks

---
+++ dwhooks-clean.h
@@ -0,0 +1,15 @@
+#ifndef libdw_h_t_
+#define libdw_h_t_
+
+typedef __uint64_t uint64_t;
+
+//static void *malloc_hook (size_t, const void *);
+
+//static void *(*old_malloc_hook) (size_t, const void *);
+
+//extern uint64_t memory_access(uint64_t, uint64_t);
+//extern uint64_t memory_access_dbg(uint64_t,uint64_t);
+extern void dw_init (void);
+//extern void final_check(void);
+
+#endif
